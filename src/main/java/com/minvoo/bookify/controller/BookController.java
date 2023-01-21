c2db0c4bd0761748c82f6c4297f8344d16b4caa0
minvoo <maciejewski_mariusz@wp.pl>
2023-01-21 21:14:12 +0100

    book controller

---
+++ src/main/java/com/minvoo/bookify/controller/BookController.java
@@ -0,0 +1,25 @@
+package com.minvoo.bookify.controller;
+
+import com.minvoo.bookify.model.Book;
+import com.minvoo.bookify.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+@RequestMapping("/api/books")
+public class BookController {
+
+    private BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PutMapping("/secure/checkout")
+    public Book checkoutBook(@RequestParam Long bookId) throws Exception {
+        String userEmail = "testuser@mail.com";
+        return bookService.checkoutBook(userEmail, bookId);
+    }
+}
